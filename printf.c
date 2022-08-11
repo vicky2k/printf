8d4df0c561dc006390aa6a7efc16037241ccc548
vicky2k <vicspidin@gmail.com>
2022-08-10 19:03:21 -0700

    printf task

--- printf.c
+++ printf.c
@@ -1,44 +1,40 @@
 #include "main.h"
+
 /**
- * _printf - function to print anything
- * @format: types of argument passed to the function
- *
- * Return: number of cs printed
+ * _printf - Receive the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
  */
 
 int _printf(const char *format, ...)
 {
-	int check = 0, i;
-	va_list params;
-	int (*func)(va_list);
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
 
-	va_start(params, format);
-	if (!format)
+	};
+	va_list arg_list;
+
+	if (format == NULL)
+
 		return (-1);
-	for (i = 0; format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (!(format[i]))
-				return (-1);
-			func = get_func(format[i]);
-			if (!func)
-			{
-				_putchar('%');
-				_putchar('%');_putchar(format[i]);
-				check += 2;
-			}
-			else 
-				check += func(params);
 
-		}
-		else
-		{
-			_putchar(format[i]);
-			check++;
-		}
-	}
-	va_end(params);
-	return (check);
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
