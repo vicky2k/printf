ed31ed9d97aa94e48f11ee563f5c7ef892974328
vicky2k <vicspidin@gmail.com>
2022-08-10 19:16:28 -0700

    header file refactor

--- main.h
+++ main.h
@@ -1,45 +1,46 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef PRINT_F
+#define PRINT_F
 
-#define BUFSIZE MAXLENGTH
-#define END '\0'
-
-#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
-#include <string.h>
-
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_c(va_list params);
-int print_s(va_list params);
-int print_p(__attribute__((unused))va_list params);
-int print_i(va_list params);
-int print_d(va_list params);
-int (*get_func(const char a))(va_list);
-int print_i(va_list params);
-int print_d(va_list params);
-int bin_r(unsigned int num, int len);
-int print_b(va_list params);
-int print_u(va_list params);
-int print_o(va_list args);
-int rot13(va_list params);
-int print_rev(va_list params);
-
-void rev_string(char *s);
 
 /**
- *struct format - structure for printing various types
- *@select: type to print
- *@meth: function to print
- */
-
-typedef struct format
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
+struct convert
 {
-	const char select;
-	int (*meth)(va_list);
-} format_t;
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+
+int parser(const char *format, conver_t f_list[], va_list arg_list);
+int _printf(const char *format, ...);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+
+
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int);
+
 
 #endif
